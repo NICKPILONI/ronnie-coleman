5575479624cddf1497e6b846bd54e7503c9d82d1
NICKPILONI <161043982+NICKPILONI@users.noreply.github.com>
2024-11-01 10:50:47 -0300

    Add files via upload

---
+++ FERRAMENTA DE ESTUDO/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ajudante',
+    'O que o treinador de Ronnie Coleman fazia antes de ajuar ele?',
+    'Era barbeiro'
+)
+
+criaCartao(
+    'Ronnie Coleman',
+    'Qual foi o PR de Ronnie Coleman no leg press?',
+    '1.043 Kg'
+)
+
+criaCartao(
+    'Adversario',
+    'Quem era o principal adversario de Ronnie?',
+    'Arnold Schwarzenegger'
+)
+
+criaCartao(
+    'Mr. Olympia',
+    'Quantos Mr. Olympia Ronnie Coleman ganhou?',
+    '8 consecutivas (1998 a 2005)'
+)
